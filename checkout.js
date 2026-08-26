3629d029e4a1c579195bac9d077d074603c20e67
rolpanca <57642594+rolpanca@users.noreply.github.com>
2026-08-26 17:13:52 -0300

    att do pedido

--- checkout.js
+++ checkout.js
@@ -6,6 +6,10 @@ const formulario = document.getElementById('form-checkout');
 const pagamento = document.querySelectorAll('input[name="pagamento"]');
 
 
+
+
+
+
 console.log(pagamento);
 console.log(pagamento.length);
 
@@ -13,6 +17,16 @@ console.log(pagamento.length);
 const nome = document.getElementById('nome');
 const email = document.getElementById('email');
 const telefone = document.getElementById('telefone');
+
+const usuarioLogado = JSON.parse(localStorage.getItem(
+
+    'usuarioLogado')
+);
+
+if (usuarioLogado) {
+    nome.value = usuarioLogado.nome;
+    email.value = usuarioLogado.email;
+}
 
 telefone.addEventListener('input', function(){
     let valor = telefone.value;
@@ -76,6 +90,7 @@ let carrinho = [];
 function atualizarCheckout(){
 
     listaCheckout.innerHTML = '';
+
     let total = 0;
 
     carrinho.forEach(function(produto){
@@ -119,7 +134,9 @@ const chaveCarrinho = obterChaveCarrrinho();
 const carrinhoSalvo = localStorage.getItem(chaveCarrinho);
 
 if (carrinhoSalvo) {
+
     carrinho = JSON.parse(carrinhoSalvo);
+
 }else {
     carrinho = [];
 }
@@ -154,23 +171,107 @@ atualizarCheckout();
         });
 
         if (!pagamentoSelecionado) {
-            alert('Selecione um forma de pagamento!');
+            alert('Selecione uma forma de pagamento!');
 
             return;
         }
 
-        btnFinalizar.disabled = true;
-        btnFinalizar.textContent = 'Processando...';
+
+
+let totalPedido = 0;
+
+carrinho.forEach(function(produto) {
+    totalPedido += produto.preco * produto.quantidade;
+});
+
+
+let formaPagamento = '';
+
+pagamento.forEach(function(opcao){
+    if (opcao.checked) {
+
+        formaPagamento = opcao.value;
+    }
+});
+
+
+
+const numeroPedido = Math.floor(Math.random() * 900000 + 100000);
+
+const pedido = {
+
+    numero:numeroPedido,
+
+    cliente: {
+        nome: nome.value,
+        email: email.value,
+        telefone: telefone.value
+    },
+
+    endereco: {
+        cep: cep.value,
+        rua: rua.value,
+        numero: numero.value,
+        bairro: bairro.value,
+        cidade: cidade.value,
+        estado: estado.value
+    },
+
+    produto: carrinho,
+    total: totalPedido,
+    pagamento: formaPagamento
+};
+
+const usuarioLogado = JSON.parse(
+  localStorage.getItem('usuarioLogado')  
+);
+
+const chavePedidos = 'pedidos_' + usuarioLogado.email;
+
+let pedidos = JSON.parse(
+    localStorage.getItem(chavePedidos)
+
+) || [];
+
+pedidos.push(pedido);
+
+localStorage.setItem(
+    chavePedidos,
+    JSON.stringify(pedidos)
+);
+
+localStorage.setItem(
+    'pedidoAtual',
+    JSON.stringify(pedido)
+)
+
+
+
+localStorage.setItem('pedidoAtual', JSON.stringify(pedido));
+
+
+
+
+// localStorage.setItem('totalPedido', totalPedido);
+// localStorage.setItem('formaPagamento', formaPagamento);
+
+
+
+
+btnFinalizar.disabled = true;
+btnFinalizar.textContent = 'Processando...';
+
+      
 
         const chaveCarrinho = obterChaveCarrrinho();
 
-        localStorage.removeItem('chaveCarrinho');
+        localStorage.removeItem(chaveCarrinho);
 
         carrinho = [];
 
-        const numeroPedido = Math.floor(Math.random() * 900000 + 100000);
+       // const numeroPedido = Math.floor(Math.random() * 900000 + 100000);
 
-        localStorage.setItem('numeroPedido', numeroPedido);
+       // localStorage.setItem('numeroPedido', numeroPedido);
 
         setTimeout(function() {
             window.location.href = 'confirmacao.html';
--- confirmacao.js
+++ confirmacao.js
@@ -1,13 +1,25 @@
-const numeroPedido = localStorage.getItem('numeroPedido');
+const pedidoSalvo = localStorage.getItem('pedidoAtual');
 const campNumero = document.getElementById('numero-pedido');
+const campTotal = document.getElementById('total-pedido');
+const campPagamento = document.getElementById('forma-pagamento');
 
-//campNumero.textContent = '#' + numeroPedido;
 
-if (numeroPedido) {
+if (pedidoSalvo) {
+    const pedido = JSON.parse(pedidoSalvo);
     
-    campNumero.textContent = '#' + numeroPedido;
+    campNumero.textContent = '#' + pedido.numero;
+
+    const totalFormatado = Number(pedido.total)
+        .toFixed(2)
+        .replace('.', ',');
+
+        campTotal.textContent = 'R$' + totalFormatado;
+        campPagamento.textContent = pedido.pagamento;
 
 }else {
     
     campNumero.textContent = 'Não encontrado';
+    campTotal.textContent = 'Não encontrado';
+    campPagamento.textContent = 'Não encontrado';
 }
+
